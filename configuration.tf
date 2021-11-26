7721a82c769d1a249d0efa83777ac8f0f7bc5213
Malo10LeGoff <malolegoff@gmail.com>
2021-11-26 23:33:18 +0100

    first iam rights and configuration

---
+++ configuration.tf
@@ -0,0 +1,34 @@
+terraform {
+    required_providers {
+        google = {
+        source = "hashicorp/google"
+        version = "3.5.0"
+    }
+    }
+}
+
+provider "google" {
+    credentials = file("C:\\Users\\LENOVO\\Downloads\\dsn-dev-01-52cb45ee6e28.json")
+
+    project = "dsn-dev-01"
+    region  = "eu-west1"
+    zone    = "eu-west1-c"
+}
+
+resource "google_project_iam_member" "clement_rights" {
+    project = "dsn-dev-01"
+    role    = "roles/editor"
+    member  = "user:clementdarthe@gmail.com"
+}
+
+resource "google_project_iam_member" "alex_rights" {
+    project = "dsn-dev-01"
+    role    = "roles/editor"
+    member  = "user:alexandre.giraud@imt-atlantique.net"
+}
+
+resource "google_project_iam_member" "mt_rights" {
+    project = "dsn-dev-01"
+    role    = "roles/editor"
+    member  = "user:truong.minhtri3007@gmail.com"
+}
