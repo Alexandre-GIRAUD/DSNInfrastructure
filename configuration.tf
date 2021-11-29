60546d3b3976fea6b0155972d2ce77a738086e6c
unknown <alexandre.giraud18@gmail.com>
2021-11-29 21:58:00 +0100

    Add CSbucket, BQ dataset, variables.tf

--- configuration.tf
+++ configuration.tf
@@ -8,27 +8,44 @@ terraform {
 }
 
 provider "google" {
-    credentials = file("C:\\Users\\LENOVO\\Downloads\\dsn-dev-01-52cb45ee6e28.json")
+    credentials = file("C:\\Users\\Giraud-Alexandre\\.ssh\\dsn-dev-01-c54f6a31d417.json")
 
-    project = "dsn-dev-01"
-    region  = "eu-west1"
+    project = var.project_id
+    region  = var.region
     zone    = "eu-west1-c"
 }
 
 resource "google_project_iam_member" "clement_rights" {
-    project = "dsn-dev-01"
+    project = var.project_id
     role    = "roles/editor"
     member  = "user:clementdarthe@gmail.com"
 }
 
 resource "google_project_iam_member" "alex_rights" {
-    project = "dsn-dev-01"
+    project = var.project_id
     role    = "roles/editor"
     member  = "user:alexandre.giraud18@gmail.com"
 }
 
 resource "google_project_iam_member" "mt_rights" {
-    project = "dsn-dev-01"
+    project = var.project_id
     role    = "roles/editor"
     member  = "user:truong.minhtri3007@gmail.com"
+}
+
+resource "google_storage_bucket" "wildfire_dsn" {
+  name          = "wildfire_dsn"
+  location      = var.region
+  force_destroy = true
+  project = var.project_id
+  storage_class = "STANDARD"
+
+}
+
+resource "google_bigquery_dataset" "Verbatim_dataset" {
+  dataset_id = "Verbatim_dataset"
+  friendly_name = "Verbatim"
+  description = "This dataset contains sentences and their verbatimes"
+  location = var.region
+  project = var.project_id
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+description = "Google Project ID."
+type        = string
+default = "dsn-dev-01"
+}
+
+variable "region" {
+description = "Google Cloud region"
+type        = string
+default     = "EU"
+}
